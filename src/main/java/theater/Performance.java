e1f00d5a375f2c15379fa43eda687cd51fe175ca
Arshdeep <arshdeep.saini@mail.utoronto.ca>
2025-11-18 14:19:04 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,23 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private int audience;
+    private final String playID;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public String getPlayID() {
+        return playID;
     }
 }
