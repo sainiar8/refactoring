1956a780c29cb533fa13850b9cec7989c4153ad5
Arshdeep <arshdeep.saini@mail.utoronto.ca>
2025-11-19 15:33:16 -0500

    Completed step 5 of task 2.

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -13,8 +13,8 @@ public class StatementPrinter {
     private static final int TRAGEDY_EXTRA_COST_PER_PERSON = 1000;
     private static final int CENTS_IN_A_DOLLAR = 100;
 
-    private Invoice invoice;
-    private Map<String, Play> plays;
+    private static Map<String, Play> plays;
+    private final Invoice invoice;
 
     public StatementPrinter(Invoice invoice, Map<String, Play> plays) {
         this.invoice = invoice;
@@ -38,26 +38,8 @@ public class StatementPrinter {
         for (Performance p : invoice.getPerformances()) {
             final Play play = plays.get(p.getPlayID());
 
-            int thisAmount = 0;
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = BASE_SALARY;
-                    if (p.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
-                        thisAmount += TRAGEDY_EXTRA_COST_PER_PERSON * (p.getAudience() - TRAGEDY_FREE_AUDIENCE);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = Constants.COMEDY_BASE_AMOUNT;
-                    if (p.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
-                        thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
-                                + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                                * (p.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
-                    }
-                    thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * p.getAudience();
-                    break;
-                default:
-                    throw new RuntimeException(String.format("unknown type: %s", play.getType()));
-            }
+            int rslt = 0;
+            rslt = getThisAmount(p);
 
             // add volume credits
             volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
@@ -69,11 +51,45 @@ public class StatementPrinter {
             // print line for this order
             result.append(String.format(
                     "  %s: %s (%s seats)%n", play.getName(), frmt.format(
-                            thisAmount / CENTS_IN_A_DOLLAR), p.getAudience()));
-            totalAmount += thisAmount;
+                            rslt / CENTS_IN_A_DOLLAR), p.getAudience()));
+            totalAmount += rslt;
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / CENTS_IN_A_DOLLAR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private static int getThisAmount(Performance performance) {
+        int thisAmount;
+        final Play play = getPlay(performance);
+        switch (play.getType()) {
+            case "tragedy":
+                thisAmount = BASE_SALARY;
+                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                    thisAmount += TRAGEDY_EXTRA_COST_PER_PERSON * (performance.getAudience() - TRAGEDY_FREE_AUDIENCE);
+                }
+                break;
+            case "comedy":
+                thisAmount = Constants.COMEDY_BASE_AMOUNT;
+                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                    thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
+                            + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                }
+                thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
+                break;
+            default:
+                throw new RuntimeException(String.format("unknown type: %s", play.getType()));
+        }
+        return thisAmount;
+    }
+
+    /**
+      * Helper for get this amount.
+      * @param performance the performance whose play we want
+      * @return the play name
+     */
+    public static Play getPlay(Performance performance) {
+        return plays.get(performance.getPlayID());
     }
 }
